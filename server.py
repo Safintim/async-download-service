b52000ccf487dac81b765cbe6c20f3aff85b0cc7
Timur Safin <timur@getgain.ru>
2020-03-03 10:03:55 +0300

    Add implementation archivate

--- server.py
+++ server.py
@@ -1,9 +1,59 @@
-from aiohttp import web
+import os.path
+import logging
+import asyncio
 import aiofiles
+from aiohttp import web
+
+import tools
+
+
+TEMPLATE_COMMAND = 'zip - -r {}'
+
+
+logging.basicConfig(
+    format='%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s',
+    level=logging.INFO)
 
 
 async def archivate(request):
-    raise NotImplementedError
+    config = request.app['config']
+    path = '{}/{}'.format(config['FILES_DIR'], request.match_info['archive_hash'])
+
+    if not os.path.exists(path):
+        raise web.HTTPNotFound(text='Архив не существует или был удален')
+
+    response = web.StreamResponse()
+    response.headers['Content-Type'] = 'text/plain'
+    response.headers['Content-Disposition'] = 'attachment; filename={}'.format(config['ARCHIVE_NAME'])
+
+    await response.prepare(request)
+
+    proc = await asyncio.create_subprocess_shell(
+        TEMPLATE_COMMAND.format(path),
+        stdout=asyncio.subprocess.PIPE
+    )
+
+    try:
+        while True:
+            archive_chunc = await proc.stdout.readline()
+            if not archive_chunc:
+                break
+
+            await response.write(archive_chunc)
+
+            if config['LOGGING']:
+                logging.info('Sending archive chunk ...')
+
+            await asyncio.sleep(int(config['RESPONSE_DELAY']))
+    except (asyncio.CancelledError, ConnectionResetError):
+        if config['LOGGING']:
+            logging.info('Download was interrupted')
+        proc.kill()
+        raise
+    finally:
+        response.force_close()
+
+    return response
 
 
 async def handle_index_page(request):
@@ -13,7 +63,11 @@ async def handle_index_page(request):
 
 
 if __name__ == '__main__':
+    parser = tools.create_parser()
+    namespace = parser.parse_args()
+
     app = web.Application()
+    app['config'] = tools.setup_config(namespace.__dict__)
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
---
+++ tools.py
@@ -0,0 +1,36 @@
+import os
+import argparse
+import pathlib
+import yaml
+
+BASE_DIR = pathlib.Path(__file__).parent
+CONFIG_PATH = BASE_DIR / 'conf.yaml'
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--FILES_DIR', type=is_exists_dir)
+    parser.add_argument('-dl', '--RESPONSE_DELAY')
+    parser.add_argument('-n', '--ARCHIVE_NAME')
+    parser.add_argument('-l', '--LOGGING', action='store_true', default=False)
+    return parser
+
+
+def is_exists_dir(dirpath):
+    if os.path.exists(dirpath):
+        return dirpath
+    raise argparse.ArgumentTypeError('dir not found')
+
+
+def get_config_from_file(path=CONFIG_PATH):
+    with open(path) as f:
+        config = yaml.safe_load(f)
+    return config
+
+
+def setup_config(config_from_cli):
+    config_from_path = get_config_from_file()
+    for key, value in config_from_cli.items():
+        if value:
+            config_from_path[key] = value
+    return config_from_path
